fefa079588b969d116a643c30588c107d9d1a2df
MIIGUEELC <miigueel9219@gmail.com>
2025-03-06 11:35:12 +0100

    despliegue s3

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
-    },
-    plugins: [react()],
-    server: {
-      port: 5500
-    }
-  };
-});
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
